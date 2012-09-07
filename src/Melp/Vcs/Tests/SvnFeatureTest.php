028f479ba0a9b74eae993bcd8974214c4c7db416
Gerard van Helden <drm@melp.nl>
2012-09-07 11:35:47 +0200

    remove commented code

--- src/Melp/Vcs/Tests/SvnFeatureTest.php
+++ src/Melp/Vcs/Tests/SvnFeatureTest.php
@@ -37,60 +37,6 @@ class SvnFeatureTest extends AbstractFeatureTest
         shell_exec('rm -rf ' . escapeshellarg($this->repository));
     }
 
-//    /**
-//     * @test
-//     * @dataProvider implementations
-//     */
-//    function functional($impl1, $impl2) {
-//        $refl1 = new \ReflectionClass('\Melp\Vcs\\' . $impl1);
-//        $refl2 = new \ReflectionClass('\Melp\Vcs\\' . $impl2);
-//
-//        $client1 = $refl1->newInstance(new \Melp\Vcs\Svn\CliAdapter());
-//        $client1->init($this->url);
-//        $client1->pull();
-//
-//        $data1 = 'Hello';
-//        $client1->put('foo/bar.txt', $data1, 'Hello hello');
-//        $client1->push();
-//        $this->assertTrue($client1->has('foo/bar.txt', 'file'));
-//        $this->assertEquals($data1, $client1->get('foo/bar.txt'));
-//
-//        $client2 = $refl2->newInstance(new \Melp\Vcs\Svn\CliAdapter());
-//        $client2->init($this->url);
-//        $this->assertTrue($client2->has('foo/bar.txt', 'file'));
-//        $this->assertEquals($data1, $client2->get('foo/bar.txt'));
-//
-//        $data2 = 'Goodbye';
-//        $client2->put('foo/bar.txt', $data2, "You say hello, I say goodbye");
-//
-//        if ($impl1 == 'Svn') {
-//            // local svn checkout should not have been changed yet
-//            $this->assertEquals($data1, $client1->get('foo/bar.txt'));
-//        }
-//        $client2->push();
-//
-//        $client1->pull();
-//        $this->assertEquals($data2, $client1->get('foo/bar.txt'));
-//
-//        $client1->rm('foo/bar.txt', 'Delete this');
-//        $client1->push();
-//
-//        $client2->pull();
-//        $this->assertTrue(!$client2->get('foo/bar.txt'));
-//
-//        $client2->branch('qux');
-//
-//        $client1->checkout('qux');
-//        $client1->put("foo/bar/baz.txt", "Waaa", "Created baz.txt");
-//        $client1->push();
-//
-//        $client2->pull();
-//        $this->assertEquals('Waaa', $client2->get("foo/bar/baz.txt"));
-//        $client2->checkout(null);
-//        $this->assertEquals(null, $client2->get("foo/bar/baz.txt"));
-//        $client2->checkout('qux');
-//        $this->assertEquals('Waaa', $client2->get("foo/bar/baz.txt"));
-//    }
 
 
     function implementations() {
